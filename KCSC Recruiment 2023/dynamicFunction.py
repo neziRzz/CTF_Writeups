ce231914191d717f350bd7fe8583422fb73f51f0
neziRzz <126742756+neziRzz@users.noreply.github.com>
2024-05-31 14:47:47 +0700

    Update dynamicFunction.py

--- KCSC Recruiment 2023/dynamicFunction.py
+++ KCSC Recruiment 2023/dynamicFunction.py
@@ -1,9 +1,9 @@
 def swapNibbles(x):
     return ( (x & 0x0F)<<4 | (x & 0xF0)>>4 )
 encrypt = ["a"]*24
-encrypt_ = "44 93 51 42 24 45 2E 9B 01 99 7F 05 4D 47 25 43 A2 E2 3E AA 85 99 18 7E"
+dest = "44 93 51 42 24 45 2E 9B 01 99 7F 05 4D 47 25 43 A2 E2 3E AA 85 99 18 7E"
 key = "reversing_is_pretty_cool"
-b = bytes.fromhex(encrypt_)
+b = bytes.fromhex(dest)
 for i in range(len(b)):
     encrypt[i] = b[i] ^ ord(key[i])
     encrypt[i] = swapNibbles(encrypt[i])
